a0e7ce70eba00fceb148e6d15ad956f04d30099f
DeansListNG <odanieludo@gmail.com>
2025-04-03 11:42:14 +0100

    updated meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ const newEditorial = localFont({
 
 
 export const metadata = {
-  title: "Dean\'slist Network State",
+  title: "Isand DAO",
   description: "A Network State Of Web3 Power users",
 };
 
@@ -40,14 +40,14 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="A Network State Of Web3 Power users" />
-        <meta name="keywords" content="Dean'slist, Network State, Web3, DAO, Power Users, Blockchain, Community, Dean, Solana, Dean'slist NG, Dean'slist BR" />
-        <meta property="og:title" content="Dean'slist Network State" />
+        <meta name="keywords" content="Dean'slist, Network State, Web3, DAO, Power Users, Blockchain, Community, Isand DAO, Solana, Dean'slist NG, Dean'slist BR" />
+        <meta property="og:title" content="Isand DAO" />
         <meta property="og:description" content="A Network State Of Web3 Power users" />
         <meta property="og:image" content="image/svg+xml" />
         <meta property="og:url" content="https://deanslist.services" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@deanslistDAO" />
-        <meta name="twitter:title" content="Dean'slist Network State" />
+        <meta name="twitter:title" content="Isand DAO" />
         <meta name="twitter:description" content="A Network State Of Web3 Power users" />
         <meta name="twitter:image" content="image/svg+xml" />
       </head>
