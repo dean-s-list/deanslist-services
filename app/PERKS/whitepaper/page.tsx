03c4a792598a4579aae918109c25599825ca4650
krkbheet@gmail.com <krkbheet@gmail.com>
2025-04-18 16:04:37 +0200

    fix: zooming whitepaper

--- app/PERKS/whitepaper/page.tsx
+++ app/PERKS/whitepaper/page.tsx
@@ -8,12 +8,12 @@ export default function WhitepaperPage() {
   return (
     <div style={{ width: '100%', height: '100vh', padding: '1rem' }}>
       <iframe
-        src="https://drive.google.com/file/d/1_x2rvT4BaZOMU0zDvzKHv1dADDegmYJF/preview"
-        width="100%"
-        height="100%"
-
-        style={{ border: 'none' }}
+        src={`https://docs.google.com/gview?url=${encodeURIComponent(
+          'https://drive.google.com/uc?export=download&id=1_x2rvT4BaZOMU0zDvzKHv1dADDegmYJF'
+        )}&embedded=true`}
+        style={{ width: '100%', height: '100vh', border: 'none' }}
       />
+
     </div>
   )
 }
