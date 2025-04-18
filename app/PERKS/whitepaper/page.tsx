e4adc5c0651f62445aff1ac27afe1a1a5c96a5ea
krkbheet@gmail.com <krkbheet@gmail.com>
2025-04-18 16:56:32 +0200

    fix: update whitepaper

--- app/PERKS/whitepaper/page.tsx
+++ app/PERKS/whitepaper/page.tsx
@@ -9,7 +9,7 @@ export default function WhitepaperPage() {
     <div style={{ width: '100%', height: '100vh', padding: '1rem' }}>
       <iframe
         src={`https://docs.google.com/gview?url=${encodeURIComponent(
-          'https://drive.google.com/uc?export=download&id=1_x2rvT4BaZOMU0zDvzKHv1dADDegmYJF'
+          'https://drive.google.com/uc?export=download&id=15cgCJktPedNdI8U-96UGQcIdUoritppn'
         )}&embedded=true`}
         style={{ width: '100%', height: '100vh', border: 'none' }}
       />
