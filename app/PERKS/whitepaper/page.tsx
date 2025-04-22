634a6fbfff2a9a359e9ab34aa86322dea5ca943d
krkbheet@gmail.com <krkbheet@gmail.com>
2025-04-22 11:58:45 +0200

    chore: fix whitepaper

--- app/PERKS/whitepaper/page.tsx
+++ app/PERKS/whitepaper/page.tsx
@@ -11,9 +11,9 @@ export default function WhitepaperPage() {
         src={`https://docs.google.com/gview?url=${encodeURIComponent(
           'https://drive.google.com/uc?export=download&id=1ZoIIRxpDdfL0jhr1_FwcinxqMi1wXUIq'
         )}&embedded=true`}
-        style={{ width: '100%', height: '100vh', border: 'none' }}
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        title="Whitepaper Viewer"
       />
-
     </div>
   )
 }
